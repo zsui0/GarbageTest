cd838b3173d0a2a2e3baa1014330e98ffaeb2992
user <user@DESKTOP-61LJDAO>
2022-09-15 09:22:41 +0200

    Add project files.

---
+++ GarbageCollecter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GarbageCollecter
+{
+    class Garbage {
+        static int Counter = 0;
+        public Garbage() {
+            Counter++;
+            Console.WriteLine("Példányok száma: {0}",Counter);
+        }
+        ~Garbage(){
+            Counter--;
+            Console.WriteLine("Példányok száma: {0}", Counter);
+        }
+    }
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            while (true) {
+                Garbage seged = new Garbage(); // 172115
+            }
+        }
+    }
+}
